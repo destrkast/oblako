a7b5773b9f39e543ad1358577a48ec7ea5ef4057
destrkast <destrkast29@gmail.com>
2021-06-25 17:25:23 +0300

    API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,2 @@
 class ApplicationController < ActionController::API
-    respond_to :json
-
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-
-  private
-
-  def record_not_found(err)
-    render json: { error: err }, status: 404
-  end
 end
